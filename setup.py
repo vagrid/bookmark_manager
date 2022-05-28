2fd7f6ce04ecc706630b2d81332d7d5889f10b95
vagrid <anthonyrey.simonnot@gmail.com>
2022-05-28 17:50:37 +0200

    Creation of the package bookmark_manager - Simple setup.py - Empty bookmark_manager repository

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name             = "bookmark_manager",
+  version          = "0.1.0",
+  description      = "Manage bookmarks",
+  long_description = "Manage bookmarks using Python",
+  packages         = ["bookmark_manager"],
+)
